e4910f08dec0d9932d2199cf2b77d3670bbe8911
suin <suinyeze@gmail.com>
2012-09-22 02:25:18 +0900

    import

---
+++ html/themes/basic5a/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="basic5a"
+Depends=Legacy_RenderSystem,legacy
+Url="http://xoops123.com/"
+Version="0.05"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XCL html5 Style"
+Author=marine/mistgreen
+ScreenShot="screenshot.png"
+Description="1-3 column , Responsive Web Design"
+W3C=YES
+
+Licence="MIT"
+
+*/
+?>
