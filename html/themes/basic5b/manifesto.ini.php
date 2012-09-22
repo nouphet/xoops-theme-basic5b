46298a1da581016066aa56384a5ea3b26c0e5d29
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2012-09-22 12:14:47 +0900

    change manifesto

--- html/themes/basic5b/manifesto.ini.php
+++ html/themes/basic5b/manifesto.ini.php
@@ -2,17 +2,18 @@
 /**
 
 [Manifesto]
-Name="basic5a"
+Name="basic5b"
 Depends=Legacy_RenderSystem,legacy
-Url="http://xoops123.com/"
-Version="0.05"
+Url="http://https://github.com/nouphet/xoops-theme-basic5b/"
+Version="1.00"
 
 [Theme]
 RenderSystem=Legacy_RenderSystem
 Format="XCL html5 Style"
 Author=marine/mistgreen
 ScreenShot="screenshot.png"
-Description="1-3 column , Responsive Web Design"
+Description="fork from basic5a(created by marine. thx marine)
+1-3 column , Responsive Web Design"
 W3C=YES
 
 Licence="MIT"
