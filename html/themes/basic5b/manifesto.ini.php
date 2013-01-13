1276c7175f7f3f8ccf5763ac38f1354e7e20168e
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2013-01-14 03:19:17 +0900

    mod pico template
    change pico editor from fckeditor to ckeditor4

--- html/themes/basic5b/manifesto.ini.php
+++ html/themes/basic5b/manifesto.ini.php
@@ -5,12 +5,12 @@
 Name="basic5b"
 Depends=Legacy_RenderSystem,legacy
 Url="http://https://github.com/nouphet/xoops-theme-basic5b/"
-Version="1.00"
+Version="1.01"
 
 [Theme]
 RenderSystem=Legacy_RenderSystem
 Format="XCL html5 Style"
-Author=marine/mistgreen
+Author=Toshihiro Takehara aka nouphet
 ScreenShot="screenshot.png"
 Description="fork from basic5a(created by marine. thx marine)
 1-3 column , Responsive Web Design"
