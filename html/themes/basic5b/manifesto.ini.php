f10f1c4af7b5b70b1c16c7fff6328927a783a69c
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2014-02-24 00:10:55 +0900

    mod ul marging and other somethings

--- html/themes/basic5b/manifesto.ini.php
+++ html/themes/basic5b/manifesto.ini.php
@@ -5,14 +5,14 @@
 Name="basic5b"
 Depends=Legacy_RenderSystem,legacy
 Url="http://https://github.com/nouphet/xoops-theme-basic5b/"
-Version="1.01"
+Version="1.02"
 
 [Theme]
 RenderSystem=Legacy_RenderSystem
 Format="XCL html5 Style"
 Author=Toshihiro Takehara aka nouphet
 ScreenShot="screenshot.png"
-Description="fork from basic5a(created by marine. thx marine)
+Description="fork from basic5a(created by marine)
 1-3 column , Responsive Web Design"
 W3C=YES
 
